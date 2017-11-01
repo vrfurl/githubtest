b445eab36f4973b981b62694a03db5dd662e1ccf
vince <vrfurl@gmail.com>
2017-11-01 15:22:22 -0700

    First commit

---
+++ src/main/java/com/github/test/githubtest/App.java
@@ -0,0 +1,13 @@
+package com.github.test.githubtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
