7e91f8c3ba63b5e1a2f27fc7a82e51f257b93d2d
vrfurl <vrfurl@gmail.com>
2017-11-01 17:19:56 -0700

    Checkin test

--- src/main/java/com/github/test/githubtest/App.java
+++ src/main/java/com/github/test/githubtest/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println( "Hello World  with a little Twist" );
+        System.out.println( "Hello World  with a little Twist" );
     }
 }
